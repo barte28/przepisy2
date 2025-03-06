7b45dc006eeae9f294cd2b597255392abcebacaa
barte28 <bartek.teuerle@gmail.com>
2025-03-06 08:57:13 +0100

    koniec lekcji

--- app/src/main/java/com/example/przepisy/ListaPrzepisowActivity.java
+++ app/src/main/java/com/example/przepisy/ListaPrzepisowActivity.java
@@ -2,10 +2,18 @@ package com.example.przepisy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class ListaPrzepisowActivity extends AppCompatActivity {
+    private ListView listViewPrzepisy;
+    private ArrayAdapter<Przepis> arrayAdapter;
+    private ArrayList<Przepis> wybranePrzepisy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +22,16 @@ public class ListaPrzepisowActivity extends AppCompatActivity {
         String kategoria = getIntent().getExtras().get(MainActivity.KATEGORIA).toString();
         TextView textView = findViewById(R.id.textViewNazwaKategori);
         textView.setText(kategoria);
+
+        wybranePrzepisy = RepozytoriumPrzepisow.wypiszPrzepisy(kategoria);
+        arrayAdapter = new ArrayAdapter<>(
+                ListaPrzepisowActivity.this,
+                android.R.layout.simple_list_item_1,
+                wybranePrzepisy
+        );
+        listViewPrzepisy = findViewById(R.id.listViewPrzepisy);
+        listViewPrzepisy.setAdapter(arrayAdapter);
+
+        Intent intent = new Intent(ListaPrzepisowActivity.this, )
     }
 }
